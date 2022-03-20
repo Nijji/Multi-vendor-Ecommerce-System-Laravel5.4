cb6500120de56bfcf012afd1185d945265bb3ea7
Nijji <alwinsenior@outlook.com>
2022-03-20 17:31:36 +0300

    updated auth/route files

---
+++ Ecommerce System Management/app/Http/Middleware/TrimStrings.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\TrimStrings as BaseTrimmer;
+
+class TrimStrings extends BaseTrimmer
+{
+    /**
+     *
+     * @var array
+     */
+    protected $except = [
+        'password',
+        'password_confirmation',
+    ];
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Ecommerce-System-using-Laravel5.4
+Ecommerce system using Laravel
+
+This project is under development 
+
+UML for this project:
+
+![alt tag](https://github.com/nijji)
